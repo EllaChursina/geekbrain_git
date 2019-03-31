5c9506ea0a7b14418b9e88d33ff552561714cd64
Ella Churina <ellachur@gmail.com>
2019-03-31 15:09:57 +0300

    Сделала домашнее задание к уроку №1

---
+++ testtest/testtest/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  testtest
+//
+//  Created by MacBook on 29/03/2019.
+//  Copyright © 2019 EllaChursina. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+

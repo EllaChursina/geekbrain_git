ac1a3445f5cf645772341850cb2b3f7838953915
Ella Churina <ellachur@gmail.com>
2019-04-14 16:21:56 +0300

    Добавила домашнее задание по пятому уроку

---
+++ l5_ChursinaElla/l5_ChursinaElla/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  l5_ChursinaElla
+//
+//  Created by MacBook on 14/04/2019.
+//  Copyright © 2019 EllaChursina. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
